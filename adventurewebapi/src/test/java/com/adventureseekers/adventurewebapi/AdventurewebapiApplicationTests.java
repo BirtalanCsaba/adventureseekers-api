039492195370fc41977ba8d7b5a9461fa52436cb
911_Birtalan_Csaba <csababirtalan259@yahoo.com>
2021-08-27 12:04:32 +0300

    created new project

---
+++ adventurewebapi/src/test/java/com/adventureseekers/adventurewebapi/AdventurewebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adventureseekers.adventurewebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventurewebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
